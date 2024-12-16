e5af0cf8bc72e15f48e42c0b433565dc2dd1a822
SoneViconia <rewejkem@gmail.com>
2024-12-16 22:17:51 +0100

    Update 2024.py

--- console-art/2024.py
+++ console-art/2024.py
@@ -1,4 +1,3 @@
-# Pixel Art for "2024" in the console
 def print_pixel_art():
     art = [
         "  #####     #####    #####    ##   ##    ",
@@ -10,10 +9,7 @@ def print_pixel_art():
         " #######    #####   #######        ##    "
     ]
     
-    # Print each line of the pixel art
     for line in art:
         print(line)
-
-# Call the function to display the art
 if __name__ == "__main__":
     print_pixel_art()
